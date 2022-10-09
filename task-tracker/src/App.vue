5d587d1eb1e35603fd153d08ecadda9b881055a2
Alkin <a.maystorov@vehiculum.de>
2022-10-09 18:12:31 +0200

    create basic task tracker app

--- task-tracker/src/App.vue
+++ task-tracker/src/App.vue
@@ -1,11 +1,78 @@
 <template>
-  <h1>Hello Vue</h1>
+  <div class="container">
+    <Header
+      :showAddTask="showAddTask"
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker" />
+
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks" />
+  </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+import Tasks from './components/Tasks.vue';
+import AddTask from './components/AddTask.vue';
+
 export default {
   name: 'App',
-  components: {},
+  components: {
+    Header,
+    AddTask,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    };
+  },
+  methods: {
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+    deleteTask(id) {
+      if (confirm(`Are you sure you want to delete the task with id ${id}?`))
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'October 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting with Product team',
+        day: 'October 2nd at 3:30pm',
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: 'Meeting with UI/UX team',
+        day: 'October 3rd at 4:30pm',
+        reminder: true,
+      },
+    ];
+  },
 };
 </script>
 
@@ -16,9 +83,11 @@ export default {
   margin: 0;
   padding: 0;
 }
+
 body {
   font-family: 'Poppins', sans-serif;
 }
+
 .container {
   max-width: 500px;
   margin: 30px auto;
@@ -28,9 +97,10 @@ body {
   padding: 30px;
   border-radius: 5px;
 }
+
 .btn {
   display: inline-block;
-  background: #000;
+  background: dodgerblue;
   color: #fff;
   border: none;
   padding: 10px 20px;
@@ -41,12 +111,15 @@ body {
   font-size: 15px;
   font-family: inherit;
 }
+
 .btn:focus {
   outline: none;
 }
+
 .btn:active {
   transform: scale(0.98);
 }
+
 .btn-block {
   display: block;
   width: 100%;
---
+++ task-tracker/src/components/Task.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    @click="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>
+      {{ task.text }}
+      <span @click="$emit('delete-task', task.id)" class="delete">X</span>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<style scope>
+.delete {
+  transform: translateY(35%);
+  color: tomato;
+}
+
+.task {
+  background: #f4f4f4;
+  margin: 5px 5px 15px 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid limegreen;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
